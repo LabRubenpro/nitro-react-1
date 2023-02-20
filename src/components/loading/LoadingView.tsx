e5e46e650b9ba46f414910f18ce4495d96fd726a
Prohotel <info@prohotel.nl>
2023-02-20 21:34:02 +0100

    Chat Fix - scroll

--- src/components/loading/LoadingView.tsx
+++ src/components/loading/LoadingView.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
 import { Base, Column, LayoutProgressBar, Text } from '../../common';
 
+var randomImage = 'loadingPhoto position-absolute';
+const randomImg: number = Math.floor(Math.random() * 30);
+
 interface LoadingViewProps
 {
     isError: boolean;
@@ -8,28 +11,34 @@ interface LoadingViewProps
     percent: number;
 }
 
-export const LoadingView: FC<LoadingViewProps> = props =>
+export const LoadingView: FC<LoadingViewProps> = (props) =>
 {
     const { isError = false, message = '', percent = 0 } = props;
     
     return (
-        <Column fullHeight position="relative" className="nitro-loading">
-            <Base fullHeight className="container h-100">
-                <Column fullHeight alignItems="center" justifyContent="end">
-                    <Base className="connecting-duck" />
-                    <Column size={ 6 } className="text-center py-4">
-                        { isError && (message && message.length) ?
-                            <Base className="fs-4 text-shadow">{ message }</Base>
-                            :
-                            <>
-                                <Text fontSize={ 4 } variant="white" className="text-shadow">{ percent.toFixed() }%</Text>
-                                <LayoutProgressBar progress={ percent } className="mt-2 large" />
-                            </>
-                        }
-                        
-                    </Column>
-                </Column>
-            </Base>
+        <Column fullHeight alignItems="center" justifyContent="center" className="nitro-loading">
+            <div className="loading-stories">
+                <div className="loadingPhoto position-absolute" data-image={ randomImg } />
+                <div className="imageOverlay position-absolute"></div>
+            </div>
+            <Column className="text-center py-4">
+                { isError && message && message.length ? (
+                    <Base className="fs-4 text-shadow">{ message }</Base>
+                ) : (
+                    <>
+                        <Base className="fs-4 text-shadow">{ message }</Base>
+                        <div className="nitro-loading-bar mt-2">
+                            <div
+                                className="nitro-loading-bar-inner"
+                                style={ { width: `${ percent }%` } }
+                            />
+                        </div>
+                        <div className="percent">
+                            <p>{ percent.toFixed() }%</p>
+                        </div>
+                    </>
+                ) }
+            </Column>
         </Column>
     );
-}
+};
